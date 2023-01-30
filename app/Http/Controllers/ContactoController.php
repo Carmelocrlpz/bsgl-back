2f36d65c8f139cb6316cf59862ec8016b7bc4fea
Miguel Emmanuel <sombrass46@gmail.com>
2023-01-29 20:17:42 -0600

    rutas rest login

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactoController extends Controller
+{
+    //
+    public function pruebas(Request $request){
+        return "action of try for CONTACTO contrller";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +20,30 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+/* Route::get('/pruebas/{nombre?}',function($nombre =null)
+  {
+  $texto='<h2>Texto de prueba</h2>';
+  $texto.='Nombre: '.$nombre;
+
+  return view('pruebas',array(
+  'texto' => $texto
+  ));
+  }); */
+
+
+Route::get('/animales', 'App\Http\Controllers\PruebasController@index');
+Route::get('/test-orm', 'App\Http\Controllers\PruebasController@testOrm');
+
+//RUTAS DEL API
+Route::get('/user/pruebas', 'App\Http\Controllers\UserController@pruebas');
+Route::get('/contacto/pruebas', 'App\Http\Controllers\ContactoController@pruebas');
+
+///rutas si sirven controlador usuarios
+Route::post('/api/register', 'App\Http\Controllers\UserController@register');
+Route::post('/api/login', 'App\Http\Controllers\UserController@login');
+Route::put('/api/user/update', 'App\Http\Controllers\UserController@update');
+Route::get('/api/user/detail/{id}', 'App\Http\Controllers\UserController@detail');
+
+
+//rutas del controlador de contacto
